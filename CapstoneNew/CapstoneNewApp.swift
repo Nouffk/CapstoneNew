5d0033ebfcb19a0bb0af27854b87618895f3c966
Nouf <nalknedri@stu.kau.edu.sa>
2024-05-22 00:51:36 +0300

    Initial Commit

---
+++ CapstoneNew/CapstoneNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneNewApp.swift
+//  CapstoneNew
+//
+//  Created by Nouf Faisal  on 14/11/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
